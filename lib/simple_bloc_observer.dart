af769568bfc5e021697492147104fec9f6460ec3
Adham-Amin <adhm0amin@gmail.com>
2024-10-19 13:58:01 +0300

    use Bloc observer

--- lib/simple_bloc_observer.dart
+++ lib/simple_bloc_observer.dart
@@ -1,35 +1,24 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 
-
-class SimpleBlocObserver implements BlocObserver
-{
+class SimpleBlocObserver implements BlocObserver {
   @override
   void onChange(BlocBase bloc, Change change) {
     debugPrint('change $change');
   }
 
   @override
-  void onClose(BlocBase bloc) {
-    // TODO: implement onClose
-  }
+  void onClose(BlocBase bloc) {}
 
   @override
-  void onCreate(BlocBase bloc) {
-    // TODO: implement onCreate
-  }
+  void onCreate(BlocBase bloc) {}
 
   @override
-  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
-    // TODO: implement onError
-  }
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {}
 
   @override
-  void onEvent(Bloc bloc, Object? event) {
-    // TODO: implement onEvent
-  }
+  void onEvent(Bloc bloc, Object? event) {}
 
   @override
-  void onTransition(Bloc bloc, Transition transition) {
-    // TODO: implement onTransition
-  }}
+  void onTransition(Bloc bloc, Transition transition) {}
+}
